d8ea44ea928f60511b849f934eea3f802017b1d2
felrando <felice.rando@gmail.com>
2024-10-29 18:55:17 -0700

    steak_templates_3b

---
+++ RScript2.R
@@ -0,0 +1,14 @@
+#rmarkdown::render("RealSteak_parameters.Rmd",
+#                  params = list(region = "Mountain"))
+
+render_report <-function(regionvar){
+  template <-"steakArticleParams.Rmd"
+  outfile <-sprintf("steakArticle_%s.html",regionvar)
+  parameters <-list(region = regionvar)
+  rmarkdown::render(template,
+                    output_file=outfile,
+                    params=parameters)
+  invisible(TRUE)
+}
+
+#render_report("Pacific")
