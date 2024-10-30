1fb3fd2d819282ec8a6ee675fd28cdb2cf69179f
felrando <felice.rando@gmail.com>
2024-10-29 18:56:22 -0700

    steak_templates_3b_addedRscript2

--- RScript2.R
+++ RScript2.R
@@ -2,13 +2,22 @@
 #                  params = list(region = "Mountain"))
 
 render_report <-function(regionvar){
-  template <-"steakArticleParams.Rmd"
+  template <-"RealSteak_parameters.Rmd"
   outfile <-sprintf("steakArticle_%s.html",regionvar)
   parameters <-list(region = regionvar)
   rmarkdown::render(template,
                     output_file=outfile,
-                    params=parameters)
+                   RealSteak_parameters params=parameters)
   invisible(TRUE)
 }
 
 #render_report("Pacific")
+
+
+library(purrr)
+params_list <- list(list("East North Central",
+                         "East South Central", "Middle Atlantic",
+                         "Mountain", "New England", "Pacific",
+                         "South Atlantic", "West North Central",
+                         "West South Central"))
+purrr::pmap(params_list,render_report)
